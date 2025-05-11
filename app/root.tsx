3ce5a2a05a433516a6cb28506f89f85fe06fc331
williams bolu <williams.bolu99@gmail.com>
2025-05-11 11:51:18 +0100

    routing

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,10 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY); // REGISTER FOR USAGE OF SYNC-FUSION COMPONENTS
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
