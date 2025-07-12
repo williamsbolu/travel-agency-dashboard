a74abde0a286b0b52deeb74961f90a82e9fd8147
williams bolu <williams.bolu99@gmail.com>
2025-07-12 02:57:09 +0100

    added travel detail page

--- app/routes.ts
+++ app/routes.ts
@@ -15,10 +15,14 @@ export default [
     route("trips/create", "./routes/admin/create-trip.tsx"),
     route("trips/:tripId", "./routes/admin/trip-detail.tsx"),
   ]),
-  layout("routes/root/page-layout.tsx", [index("routes/root/travel-page.tsx")]),
+  layout("routes/root/page-layout.tsx", [
+    index("routes/root/travel-page.tsx"),
+    route("/travel/:tripId", "routes/root/travel-detail.tsx"),
+    route("/travel/:tripId/success", "routes/root/payment-success.tsx"),
+  ]),
 ] satisfies RouteConfig;
 
 // on the package.json file: because of the sentry integration which added values to the scripts, there where changes to the "dev" and "start" scripts:
 // so i used cross-env The simplest and most reliable way to set environment variables in npm scripts across different operating systems (Windows, macOS, Linux) because i was getting errors when running the dev script on windows
-// without cross-env i can use this crip below but only works on windows:
+// without cross-env i can use this script below but only works on windows:
 // "dev": "set NODE_OPTIONS=--import ./instrument.server.mjs && react-router dev", "start": "set NODE_OPTIONS=--import ./instrument.server.mjs && react-router-serve ./build/server/index.js",
